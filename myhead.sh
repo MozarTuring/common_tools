4c4fe7d8779ec5d5a46e3f64b29d1486e851a4a7
maojingwei@10.20.14.43 <jingwei_m@163.com>
2023-08-03 09:41:19 +0800

    v14

--- myhead.sh
+++ myhead.sh
@@ -1,11 +1,6 @@
 cd $2
 pwd
 
-version=3.8.16
-
-if [ $# -eq 3 ];then
-    verion=$3
-fi
 
 if [ "$1" == "pyenv" ]; then
     if [ ! -d "pyenv" ]; then
@@ -16,7 +11,7 @@ if [ "$1" == "pyenv" ]; then
 elif [ "$1" == "condaenv" ]; then
     if [ ! -d "condaenv" ]; then
         echo "create condaenv"
-        /home/maojingwei/installed/anaconda3/bin/conda create -p condaenv python=$version
+        /home/maojingwei/installed/anaconda3/bin/conda create -p condaenv python=$3
     fi
     source /home/maojingwei/installed/anaconda3/bin/activate $2/condaenv
 fi
