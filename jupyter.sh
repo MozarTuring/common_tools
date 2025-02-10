840b7f70c4fa52c082b3199482a02dfbc346b9ce
maojingwei <jingwei_m@163.com>
2025-02-10 15:39:26 +0800

    v

--- jupyter.sh
+++ jupyter.sh
@@ -1,5 +1,4 @@
 jupyter notebook --generate-config
 jupyter notebook password
-cp ~/project/common_tools/jupyter_notebook_config.py ~/.jupyter/jupyter_notebook_config.py
-cat ~/.jupyter/jupyter_notebook_config.json >> ~/.jupyter/jupyter_notebook_config.py
-jupyter notebook --config=~/project/common_tools/jupyter_notebook_config.py --ip=0.0.0.0 --port=8888 --no-browser
+
+jupyter notebook --ip=0.0.0.0 --port=8888 --no-browser
