241664beb2286745bfbd196e3cb0dea6b43583c9
MozarTuring <marvin.maojw95@gmail.com>
2026-05-31 12:25:49 +0200

    v

--- meta_script.sh
+++ meta_script.sh
@@ -256,10 +256,6 @@ if [[ $# -lt 3 ]]; then
                 -o ServerAliveInterval=30 -o ServerAliveCountMax=3 \
                 -o ExitOnForwardFailure=yes "$host" && echo "$port succeed" || echo "Port $port tunnel failed/active"
 
-            ssh -o ControlPath=none -f -N -L "3031:localhost:3031" \
-                -o ServerAliveInterval=30 -o ServerAliveCountMax=3 \
-                -o ExitOnForwardFailure=yes jinma@ferragon.stellar.research.liu.se && echo "$port succeed" || echo "Port $port tunnel failed/active"
-
         done
     done
     _abspath="$1"
