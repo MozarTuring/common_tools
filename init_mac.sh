405d72554288c2b9c5a4198668542c03635a31d1
MozarTuring <marvin.maojw95@gmail.com>
2026-06-01 15:42:22 +0200

    v

--- init_mac.sh
+++ init_mac.sh
@@ -1,9 +1,8 @@
 # install kitty 
 
-export homedir=/Users/jinma63
 # zsh
 rm ~/.zshrc
-ln -s ${homedir}/project/common_tools/zshrc ~/.zshrc
+ln -s ${HOME}/project/common_tools/zshrc ~/.zshrc
 
 mkdir -p ~/111mjw_tmp_jwm
 alias rm='f() { DIR=~/111mjw_tmp_jwm/trash/$(date +%F%T) && mkdir -p "$DIR" && mv "$@" "$DIR"; }; f'
@@ -11,38 +10,39 @@ alias rm='f() { DIR=~/111mjw_tmp_jwm/trash/$(date +%F%T) && mkdir -p "$DIR" && m
 
 # nvim
 mkdir -p ~/.config/nvim
-ln -s ${homedir}/project/common_tools/init_nvim_mac.lua ~/.config/nvim/init.lua
+ln -s ${HOME}/project/common_tools/init_nvim_mac.lua ~/.config/nvim/init.lua
 
 # vscode
-rm ${homedir}/Library/Application\ Support/Code/User/settings.json
-ln -s ${homedir}/project/common_tools/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+rm ${HOME}/Library/Application\ Support/Code/User/settings.json
+ln -s ${HOME}/project/common_tools/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 
-rm ${homedir}/Library/Application\ Support/Code/User/keybindings.json
-ln -s ${homedir}/project/common_tools/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+rm ${HOME}/Library/Application\ Support/Code/User/keybindings.json
+ln -s ${HOME}/project/common_tools/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 
 # cursor
-rm ${homedir}/Library/Application\ Support/Cursor/User/settings.json
-ln -s ${homedir}/project/common_tools/vscode/settings.json ~/Library/Application\ Support/Cursor/User/settings.json
+rm ${HOME}/Library/Application\ Support/Cursor/User/settings.json
+ln -s ${HOME}/project/common_tools/vscode/settings.json ~/Library/Application\ Support/Cursor/User/settings.json
 
 
 # ssh
 rm ~/.ssh/config
 mkdir ~/.ssh
-ln -s ${homedir}/project/common_tools/.ssh/config ~/.ssh/config
+ln -s ${HOME}/project/common_tools/.ssh/config ~/.ssh/config
 
 # hammerspoon
 rm ~/.hammerspoon/init.lua
 mkdir ~/.hammerspoon
-ln -s ${homedir}/project/common_tools/hamperspoon.lua ~/.hammerspoon/init.lua
+ln -s ${HOME}/project/common_tools/hamperspoon.lua ~/.hammerspoon/init.lua
 
 # kitty
 rm ~/.config/kitty/kitty.conf
 mkdir -p ~/.config/kitty
-ln -s ${homedir}/project/common_tools/kitty.conf ~/.config/kitty/kitty.conf
+ln -s ${HOME}/project/common_tools/kitty.conf ~/.config/kitty/kitty.conf
 
 # git
-ln -s ${homedir}/project/common_tools/gitconfig ~/.gitconfig
+ln -s ${HOME}/project/common_tools/gitconfig ~/.gitconfig
 
 # data
-ln -s ${homedir}/Desktop/baidu/zzzjwmresources ~/project/zzzjwmresources
-ln -s ${homedir}/Desktop/baidu/zzzjwmoutput ~/project/zzzjwmoutput
+ln -s ${HOME}/Desktop/baidu/zzzjwmresources ~/project/zzzjwmresources
+ln -s ${HOME}/Desktop/baidu/zzzjwmoutput ~/project/zzzjwmoutput
+ln -s ${HOME}/Desktop/baidu/project_nogit ~/project/project_nogit
