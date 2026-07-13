451575b9f46d73b3e3a5d5fd40ecc079dba99996
MozarTuring <marvin.maojw95@gmail.com>
2026-07-13 18:07:19 +0200

    v

--- meta_script.sh
+++ meta_script.sh
@@ -622,10 +622,10 @@ EOF
     elif [[ "$1" == "remotedocker" ]]; then
         cat >>jwm_configs/remote_tmps/remote.sh <<'EOF'
 if [[ ${notebook_flag} == 1 ]]; then
-    docker rm -f ${image_name}_notebook
     echo "ARGS_AFTER_ENTRY:"
     echo "${ARGS_AFTER_ENTRY[@]}"
-    sleep 10
+    docker rm -f ${image_name}_notebook
+    sleep 5
     DOCKER_RUN_ARGS=(--name "${image_name}_notebook" -p 18889:18889 --entrypoint /bin/bash "${DOCKER_RUN_ARGS[@]}" -c "jupyter lab --ip=0.0.0.0 --port=18889 --no-browser --allow-root")
 
 else
