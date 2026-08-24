4302bcd0838a6ad303658598c66ff8bc29753bca
MozarTuring <marvin.maojw95@gmail.com>
2026-08-24 14:52:13 +0200

    v

--- sync_and_commit_repo.sh
+++ sync_and_commit_repo.sh
@@ -25,5 +25,5 @@ if [[ -n "$server_name" ]]; then
     _remote_proj="${repo_path}_${_git_branch}"
     export run_dir_remote="${run_dir_home}/project_remote_jwm/${_remote_proj}"
     echo "remote dir: ${run_dir_remote}"
-    rsync -a --exclude-from="$HOME/project/common_tools/rsync_exclude.txt" ./ "$server_name":${run_dir_remote}/
+    rsync -aL --exclude-from="$HOME/project/common_tools/rsync_exclude.txt" ./ "$server_name":${run_dir_remote}/
 fi
