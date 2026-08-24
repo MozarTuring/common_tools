bd2ebed6bc55030620c2f598b648ff082bafb4a5
MozarTuring <marvin.maojw95@gmail.com>
2026-08-24 17:11:04 +0200

    v

--- sync_and_commit_repo.sh
+++ sync_and_commit_repo.sh
@@ -30,7 +30,7 @@ if [[ -n "$server_name" ]]; then
     if [[ ! -d ${tmppath} ]]; then
         mkdir -p ${tmppath}
     fi
-    rsync -av ${tmppath} "$server_name":${run_dir_remote}/jwm_configs/
+    rsync -av --safe-links ${tmppath} "$server_name":${run_dir_remote}/jwm_configs/
     if [[ ! -L "./jwm_configs/${repo_path}" ]]; then
         ln -s ${tmppath} ./jwm_configs/${repo_path}
     fi
