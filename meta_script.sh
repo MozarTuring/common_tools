040c695e00ad8d5c474d5a8c96fcfdacdb68b679
MozarTuring <marvin.maojw95@gmail.com>
2026-08-31 10:56:57 +0200

    v

--- meta_script.sh
+++ meta_script.sh
@@ -681,9 +681,8 @@ EOF
         fi
 
         echo "JWM_RUN_COMMAND, ${JWM_RUN_COMMAND}"
-        export CUDA_VISIBLE_DEVICES='${CUDA_VISIBLE_DEVICES}'
 
-        nohup ${JWM_RUN_COMMAND} >jwmlogs/${JWM_RUN_START_TIME}/job_out.log 2>&1 &
+        CUDA_VISIBLE_DEVICES='${CUDA_VISIBLE_DEVICES}' nohup ${JWM_RUN_COMMAND} >jwmlogs/${JWM_RUN_START_TIME}/job_out.log 2>&1 &
         export JWM_JOB_ID=$!
         disown ${JWM_JOB_ID}
         cd ${RUN_DIR_HOME}/project_remote_jwm/${RUN_PROJ}
